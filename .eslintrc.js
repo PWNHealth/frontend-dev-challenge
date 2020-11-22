c9c44941dd5d17db9066eb1bc23de28a8bd8e314
Thiago Thimóteo <thiago.thimoteo@gmail.com>
2020-11-22 12:28:14 -0300

    Add eslint/prettier

---
+++ .eslintrc.js
@@ -0,0 +1,36 @@
+module.exports = {
+  'env': {
+    'browser': true,
+    'es6': true,
+    'jquery': true
+  },
+  'extends': ['plugin:prettier/recommended'],
+  'globals': {
+    'Atomics': 'readonly',
+    'SharedArrayBuffer': 'readonly'
+  },
+  'parserOptions': {
+    'ecmaVersion': 2018,
+    'sourceType': 'module'
+  },
+  'rules': {
+    'indent': [
+      'error',
+      2
+    ],
+    'linebreak-style': [
+      'error',
+      'unix'
+    ],
+    'quotes': [
+      'error',
+      'single'
+    ],
+    'semi': [
+      'error',
+      'never'
+    ],
+    'no-console': 2,
+    'max-len': ['error', { 'code': 120, 'ignoreUrls': true }]
+  }
+}
