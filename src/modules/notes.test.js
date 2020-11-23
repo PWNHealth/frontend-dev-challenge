38d8e8b3086f0425afff116c5e2588f7b40eae51
Thiago Thimóteo <thiago.thimoteo@gmail.com>
2020-11-23 11:32:39 -0300

    Improve notes specs

--- src/modules/notes.test.js
+++ src/modules/notes.test.js
@@ -1,17 +1,17 @@
-import { screen } from '@testing-library/dom'
+import { screen, fireEvent } from '@testing-library/dom'
 
 import Notes from './notes'
 
 const html = `
   <div class="segment-content notes" data-module="notes">
-    <div class="notes-group" data-target="messages"></div>
-    <form class="notes-form stackGroup" data-target="form">
+    <div class="notes-group" data-target="messages" data-testid="messages"></div>
+    <form class="notes-form stackGroup" data-target="form" data-testid="form">
       <div class="radio-group">
         <div class="radio">
           <input id="note_type_private" type="radio" name="note_type" value="private" data-target="type">
           <label for="note_type_private">Private</label>
         </div>
-        <div class="radio" >
+        <div class="radio">
           <input id="note_type_public" type="radio" name="note_type" value="public" data-target="type" checked>
           <label for="note_type_public">Public</label>
         </div>
@@ -24,7 +24,7 @@ const html = `
           maxlength="1000"
           required
         ></textarea>
-        <small data-target="counter"></small>
+        <small data-target="counter" data-testid="counter"></small>
       </div>
 
       <button class="button" data-target="submit">Add Note</button>
@@ -46,6 +46,71 @@ describe('Notes', () => {
 
       expect(counter).toBeInTheDocument()
       expect(publicInput).toBeChecked()
+    })
+  })
+
+  describe('#setCounter', () => {
+    it('shows remaining characters', () => {
+      const counter = screen.getByTestId('counter')
+      const textarea = screen.getByPlaceholderText(/Insert your note/)
+
+      fireEvent.input(textarea, { target: { value: 'Test note' } })
+
+      expect(counter).toHaveTextContent(/991 characters remaining/)
+
+      fireEvent.input(textarea, { target: { value: '' } })
+
+      expect(counter).toHaveTextContent(/1000 characters remaining/)
+    })
+  })
+
+  describe('#handleSubmit', () => {
+    it('creates a public note', () => {
+      const textarea = screen.getByPlaceholderText(/Insert your note/)
+      const form = screen.getByTestId('form')
+
+      fireEvent.input(textarea, { target: { value: 'Test note' } })
+      fireEvent.submit(form)
+
+      const note = screen.getByText('Test note')
+      const headerText = screen.getByText(/Created by the physician Jane Doe on/)
+
+      expect(textarea).toHaveValue('')
+      expect(note).toBeInTheDocument()
+      expect(headerText).toBeInTheDocument()
+    })
+
+    it('returns an error when private does not have username', () => {
+      const textarea = screen.getByPlaceholderText(/Insert your note/)
+      const form = screen.getByTestId('form')
+      const privateInput = screen.getByDisplayValue(/private/)
+
+      window.alert = jest.fn()
+
+      fireEvent.change(privateInput, { target: { checked: true } })
+      fireEvent.input(textarea, { target: { value: 'Test note' } })
+      fireEvent.submit(form)
+
+      expect(window.alert).toHaveBeenCalledWith('You have to add an username when a note is private')
+
+      expect(textarea).toHaveValue('Test note')
+    })
+
+    it('creates a private note', () => {
+      const textarea = screen.getByPlaceholderText(/Insert your note/)
+      const form = screen.getByTestId('form')
+      const privateInput = screen.getByDisplayValue(/private/)
+
+      fireEvent.change(privateInput, { target: { checked: true } })
+      fireEvent.input(textarea, { target: { value: '@JohnDoe Test note' } })
+      fireEvent.submit(form)
+
+      const note = screen.getByText(/Test note/)
+      const headerText = screen.getByText(/Created by the physician Jane Doe on/)
+
+      expect(textarea).toHaveValue('')
+      expect(headerText.innerHTML).toMatch(/<span class="tag">Private<\/span>Created by the physician Jane Doe/)
+      expect(note.innerHTML).toMatch(/<mark>@JohnDoe<\/mark> Test note/)
     })
   })
 })
